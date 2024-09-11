539f14ec337dd2f9d12561b76424562665c47999
Austin Bu <hydrfyre@gmail.com>
2024-09-11 15:38:09 -0400

    Assign01_01 - 01_03

--- assigns/assign00/lib/assign00_02.ml
+++ assigns/assign00/lib/assign00_02.ml
@@ -1,10 +1,9 @@
-open Assign00_01
-
 let is_prime n =
   let rec loop k =
     if n < 2 then false
-    else if k > sqrt n then true
+    else if n = 2 then true
     else if n mod k = 0 then false
-    else loop (k + 1)
+    else if Assign00_01.sqrt n <= k then true
+      else loop (k + 1)
   in
   loop 2
