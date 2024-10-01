08a4129a9f0676df81dcd7b09b796e5b7269f6d2
Austin Bu <hydrfyre@gmail.com>
2024-10-01 11:31:16 -0400

    4-1 mem check

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -6,11 +6,14 @@ let last_function_standing funcs start pred =
     match funcs with
     | [] -> func, same
     | head :: tail -> 
-      let rec loop func s p count =
-        if count > 1000 then 1001
-        else if p (func s) then count
-        else loop func (func s) p (count + 1)
-      in let c = loop head start pred 0
+      let rec loop func s p count seen =
+        let s = func s in
+        if List.mem s seen then
+          max_int
+        else if count > 1000 then 1001
+        else if p s then count
+        else loop func s p (count + 1) (s :: seen)
+      in let c = loop head start pred 0 []
       in if c > max
         then iterate tail start pred head c false
       else if c = max
