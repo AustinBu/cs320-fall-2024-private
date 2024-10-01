492682426b8325888448a55e68d9deea18fb7661
Austin Bu <hydrfyre@gmail.com>
2024-10-01 13:18:51 -0400

    4-1 compare large funcs to find infinite

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,4 +1,10 @@
 let last_function_standing funcs start pred =
+  let rec resolve funcs start pred =
+    let l = fst funcs (fst start), snd funcs (snd start)
+    in if pred (fst l) then snd funcs
+    else if pred (snd l) then fst funcs
+    else resolve funcs l pred
+  in
   match funcs with
   | [] -> None
   | head :: _ ->
@@ -6,18 +12,18 @@ let last_function_standing funcs start pred =
     match funcs with
     | [] -> func, same
     | head :: tail -> 
-      let rec loop func s p count seen =
-        let s = func s in
-        if List.mem s seen then
-          max_int
-        else if count > 1000 then 1001
+      let rec loop func s p count max =
+        let s = func s in 
+        if count > 1000 then 1001
         else if p s then count
-        else loop func s p (count + 1) (s :: seen)
-      in let c = loop head start pred 0 []
+        else loop func s p (count + 1) max
+      in let c = loop head start pred 0 max
       in if c > max
         then iterate tail start pred head c false
       else if c = max
-        then iterate tail start pred func max true
+        then if c = 1001
+          then iterate tail start pred (resolve (head, func) (start, start) pred) max false
+        else iterate tail start pred func max true
       else
         iterate tail start pred func max false
     in let x, y = iterate funcs start pred head (-1) false
