f73439076e86756514cdd8546c7169f2961732f0
Austin Bu <hydrfyre@gmail.com>
2024-10-01 15:54:02 -0400

    4-2

--- assigns/assign04/lib/assign04_02.ml
+++ assigns/assign04/lib/assign04_02.ml
@@ -10,38 +10,66 @@ type ty =
 | Int
 | Bool
 
-(* let type_of e = e
-  let rec bool_loop e =
-    match e with
-    | True -> true
-    | False -> false
-    | Or (x, y) -> 
-      match (x, y) with
-        | (True, _) | (_, True) -> true
-        | (False, False) -> false
-        | (_, _) -> bool_loop x || bool_loop y
-    | _ -> false
-    in
-    let rec primitive_loop e =
+let rec bool_loop e =
+  match e with
+  | Num _ | Add (_, _) -> false, false
+  | True -> true, true
+  | False -> false, true
+  | IfThenElse (x, y, z) -> 
+    let a, b = bool_loop x in
+    if b then 
+      if a then bool_loop y
+      else bool_loop z
+    else false, false
+  | Or (x, y) -> 
+    (match (x, y) with
+      | (True, False) | (False, True) | (True, True) -> true, true
+      | (False, False) -> false, true
+      | (_, _) -> 
+        let a = bool_loop x in
+        let b = bool_loop y in
+        if snd a && snd b then
+          fst a || fst b, true
+        else false, false)
+
+let rec num_loop e =
+  match e with
+  | Or (_, _) | True | False -> false
+  | Num _ -> true
+  | Add (x, y) -> num_loop x && num_loop y
+  | IfThenElse (x, y, z) ->
+    let a, b = bool_loop x in
+    if b then
+      if a
+        then num_loop y
+      else
+        num_loop z
+    else false
+(* let type_of e = 
+  match e with
+  | _ -> None *)
+
+let type_of e =
+  let rec primitive_loop e =
     match e with
     | True -> Some Bool
     | False -> Some Bool
-    | Num x -> Some Int
+    | Num _ -> Some Int
     | Or (x, y) -> 
-      match (x, y) with
-        | (True, True) | (False, True) | (True, False) | (False, False) -> Some Bool
-        |(_, _) -> None
+      (match (x, y) with
+      | (True, True) | (False, True) | (True, False) | (False, False) -> Some Bool
+      |(_, _) -> None)
     | Add (x, y) ->
-      match (x, y) with
-      | (Num _, Num _) -> Some Int
-      | (_, _) -> None
+      if num_loop x && num_loop y then Some Int
+      else None
     | IfThenElse (x, y, z) ->
-      if bool_loop x
-        then primitive_loop y
-      else
-        primitive_loop z
-    in primitive_loop e *)
-  
-let type_of e = 
-  match e with
-  | _ -> None
+      let a, b = bool_loop x in
+      if b then
+        if a
+          then primitive_loop y
+        else
+          primitive_loop z
+      else None
+    in
+  primitive_loop e
+ 
