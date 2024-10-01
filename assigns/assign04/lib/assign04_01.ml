622c11e079f4bf5c0ff5e79e790fa9b3ed8a2d24
Austin Bu <hydrfyre@gmail.com>
2024-10-01 15:00:50 -0400

    4-1 corner case

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,8 +1,11 @@
 let last_function_standing funcs start pred =
   let rec resolve funcs start pred =
-    let l = fst funcs (fst start), snd funcs (snd start)
-    in if pred (fst l) then snd funcs
-    else if pred (snd l) then fst funcs
+    let l = (fst funcs) (fst start), (snd funcs) (snd start)
+    in 
+    if pred (fst l) && pred (snd l)
+      then (fst funcs), true
+    else if pred (fst l) then (snd funcs), false
+    else if pred (snd l) then (fst funcs), false
     else resolve funcs l pred
   in
   match funcs with
@@ -22,7 +25,10 @@ let last_function_standing funcs start pred =
         then iterate tail start pred head c false
       else if c = max
         then if c = 1001
-          then iterate tail start pred (resolve (head, func) (start, start) pred) max false
+          then let x, y = resolve (head, func) (start, start) pred in
+          if y
+            then iterate tail start pred x max true
+          else iterate tail start pred x max false
         else iterate tail start pred func max true
       else
         iterate tail start pred func max false
