35405ef19cafd5ec9b2a19be40c8aeef7f409e25
Austin Bu <hydrfyre@gmail.com>
2024-10-10 15:09:54 -0400

    5-2 real

--- assigns/assign05/lib/assign05_02.ml
+++ assigns/assign05/lib/assign05_02.ml
@@ -2,4 +2,11 @@ type 'a tree =
   | Leaf
   | Node of 'a * 'a tree * 'a tree
 
-let sum_tr _ = 12345
+let sum_tr t =
+  let rec loop t cont =
+    match t with
+    | Leaf -> cont 0
+    | Node (x, l, r) ->
+        loop l (fun sum_l -> loop r (fun sum_r -> cont (x + sum_l + sum_r)))
+  in
+  loop t (fun x -> x)
